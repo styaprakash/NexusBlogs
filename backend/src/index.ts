ce86f1d0595605d0a076f5f68b7dbffa61e998bd
Satya prakash mishra <satyaprakashmishra23.12@gmail.com>
2024-07-06 21:34:42 +0530

    Created backend and Initialized datbase using Postgres and Prisma ORM.

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,9 +1,69 @@
 import { Hono } from 'hono'
+import { PrismaClient } from '@prisma/client/edge'
+import { withAccelerate } from '@prisma/extension-accelerate'
+import { decode,sign,verify } from 'hono/jwt';
 
-const app = new Hono()
+const app = new Hono<{
+	Bindings: {
+		DATABASE_URL: string
+	}
+}>();
 
-app.get('/', (c) => {
+app.post('/api/v1/signup', async (c) => {
+	const prisma = new PrismaClient({
+		datasourceUrl: c.env?.DATABASE_URL,
+	}).$extends(withAccelerate());
+
+	const body = await c.req.json();
+
+	try {
+		const user = await prisma.user.create({
+			data: {
+				email: body.email,
+				password: body.password
+			}
+		});
+	  //@ts-ignore
+    const token = await sign({id:user.id},c.env.JWT_SECRET);
+
+		return c.json({
+      jwt: token
+    })
+	} catch(e) {
+		return c.status(403);
+	}
+})
+app.post('/api/v1/signin', async (c) => {
+	const prisma = new PrismaClient({
+		datasourceUrl: c.env?.DATABASE_URL	,
+	}).$extends(withAccelerate());
+
+	const body = await c.req.json();
+	const user = await prisma.user.findUnique({
+		where: {
+			email: body.email
+		}
+	});
+
+	if (!user) {
+		c.status(403);
+		return c.json({ error: "user not found" });
+	}
+  //@ts-ignore
+	const jwt = await sign({ id: user.id }, c.env.JWT_SECRET);
+	return c.json({ jwt });
+})
+
+
+app.post('/api/v1/blog', (c) => {
   return c.text('Hello Hono!')
 })
 
+app.put('/api/v1/blog', (c) => {
+  return c.text('Hello Hono!')
+})
+
+app.get('/api/v1/blog/:id', (c) => {
+  return c.text('Hello Hono!')
+})
 export default app
