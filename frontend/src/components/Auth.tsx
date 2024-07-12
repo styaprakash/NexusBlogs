749cd6ce80a3704a3ba84e1b64b37770da8c53fa
Satya prakash mishra <satyaprakashmishra23.12@gmail.com>
2024-07-12 12:11:40 +0530

    Created Frontend

--- frontend/src/components/Auth.tsx
+++ frontend/src/components/Auth.tsx
@@ -1,27 +1,29 @@
-import { SignupInput } from "@styaprakash/nexus-common";
 import { ChangeEvent, useState } from "react";
-import { Link, Navigate, useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { SignupInput } from "@styaprakash/nexus-common";
 import axios from "axios";
 import { BACKEND_URL } from "../config";
-export const Auth = ({type}: {type:"signup" | "signin"})=> {
+
+export const Auth = ({ type }: { type: "signup" | "signin" }) => {
     const navigate = useNavigate();
-    const [postInputs,setPostInputs] = useState<SignupInput>({
+    const [postInputs, setPostInputs] = useState<SignupInput>({
         name: "",
         username: "",
         password: ""
     });
 
-    async function sendRequest(){
+    async function sendRequest() {
         try {
-            const response = await axios.post(`${BACKEND_URL}/api/v1/user/${type === "signup"?"signup" : "signin"}`, postInputs);
+            const response = await axios.post(`${BACKEND_URL}/api/v1/user/${type === "signup" ? "signup" : "signin"}`, postInputs);
             const jwt = response.data;
-            localStorage.setItem("token",jwt);
+            localStorage.setItem("token", jwt);
             navigate("/blogs");
-        }catch(e) {
-            //alert the user that the request failed
+        } catch(e) {
             alert("Error while signing up")
+            // alert the user here that the request failed
         }
     }
+    
     return <div className="h-screen flex justify-center flex-col">
         <div className="flex justify-center">
             <div>
@@ -29,30 +31,33 @@ export const Auth = ({type}: {type:"signup" | "signin"})=> {
                     <div className="text-3xl font-extrabold">
                         Create an account
                     </div>
-                    <div className="text-slate-500 ">
-                        {type === "signin" ? "Don't have an account?":"Already have an account?"}
-                        <Link className="pl-2 underline" to={type ==="signin"?"/signup":"/signin"}>
-                            {type === "signin"?"Sign up":"Sign in"}
+                    <div className="text-slate-500">
+                        {type === "signin" ? "Don't have an account?" : "Already have an account?" }
+                        <Link className="pl-2 underline" to={type === "signin" ? "/signup" : "/signin"}>
+                            {type === "signin" ? "Sign up" : "Sign in"}
                         </Link>
                     </div>
                 </div>
                 <div className="pt-8">
-                {type === "signup" ? <LabelledInput label="Name" placeholder="Satya Prakash Mishra..." onChange={(e) => {
-                    setPostInputs({...postInputs,
-                        name: e.target.value
-                    })
-                }}/>:null}
-                <LabelledInput label="Username" placeholder="satyaprakashmishra@gmail.com" onChange={(e) => {
-                    setPostInputs({...postInputs,
-                        username: e.target.value
-                    })
-                }}/>
-                <LabelledInput label="Password" type={"password"} placeholder="123456" onChange={(e) => {
-                    setPostInputs({...postInputs,
-                        password: e.target.value
-                    })
-                }}/>
-                <button onClick={sendRequest} type="button" className="mt-8 w-full text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700">{type === "signup"? "Sign up" : "Sign in"}</button>
+                    {type === "signup" ? <LabelledInput label="Name" placeholder="Harkirat Singh..." onChange={(e) => {
+                        setPostInputs({
+                            ...postInputs,
+                            name: e.target.value
+                        })
+                    }} /> : null}
+                    <LabelledInput label="Username" placeholder="harkirat@gmail.com" onChange={(e) => {
+                        setPostInputs({
+                            ...postInputs,
+                            username: e.target.value
+                        })
+                    }} />
+                    <LabelledInput label="Password" type={"password"} placeholder="123456" onChange={(e) => {
+                        setPostInputs({
+                            ...postInputs,
+                            password: e.target.value
+                        })
+                    }} />
+                    <button onClick={sendRequest} type="button" className="mt-8 w-full text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700">{type === "signup" ? "Sign up" : "Sign in"}</button>
                 </div>
             </div>
         </div>
@@ -66,9 +71,9 @@ interface LabelledInputType {
     type?: string;
 }
 
-function LabelledInput({label, placeholder, onChange, type}:LabelledInputType ){
+function LabelledInput({ label, placeholder, onChange, type }: LabelledInputType) {
     return <div>
-    <label className="block mb-2 text-sm text-black font-semibold pt-4">{label}</label>
-    <input onChange={onChange} type={type || "text"} id="first_name" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder={placeholder} required />
-</div>
+        <label className="block mb-2 text-sm text-black font-semibold pt-4">{label}</label>
+        <input onChange={onChange} type={type || "text"} id="first_name" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder={placeholder} required />
+    </div>
 }
--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -1,1 +1,1 @@
-export const  BACKEND_URL = "https://backend.satyaprakashmishra23-12.workers.dev"
+export const BACKEND_URL = "https://backend.satyaprakashmishra23-12.workers.dev"
